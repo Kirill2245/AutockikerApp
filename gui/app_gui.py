2e500c6211264e59ca9f32726d9de4c591163c22
Kirill <publickirill2245@gmail.com>
2025-08-22 22:04:48 +0500

    first commit

---
+++ gui/app_gui.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+class AppGUI:
+    def __init__(self, root, core_instance = None):
+        self.root = root
+        self.core = core_instance
+        self.setup_ui()
+    
+    def setup_ui(self):
+        self.root.title("Мое приложение")
+        self.root.geometry("400x300")
+        
+
+root = tk.Tk()
+
+app = AppGUI(root)
+root.mainloop()
